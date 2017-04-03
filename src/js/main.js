edb20a2568006233056db3f2c1cdc200ae1f508f
Hoonio <hoonio@hoonio.com>
2017-04-03 23:14:21 +0100

    map select buttons to player choice function

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,21 @@ modules.print('it works well!');
 document.getElementById("reset").addEventListener("click", () => {
   modules.resetGame();
 });
+
+const playButtons = document.getElementsByClassName("play-choice");
+
+for (var i=0; i<playButtons.length; i++){
+  const button = playButtons[i];
+  button.addEventListener("click", () => {
+    modules.playerMove(button.id);
+  });
+}
+
+// playButtons.forEach((button) => {
+//   console.log(button.id)
+// })
+
+// addEventListener("click", () => {
+//   console.log(this);
+//   modules.resetGame();
+// });
--- src/js/module.js
+++ src/js/module.js
@@ -5,7 +5,16 @@ const print = (it) => {
 const resetGame = () => {
   document.getElementById("message").innerHTML = "New game";
 	document.getElementById("reset").classList.add("hidden");
-	console.log('game reset');
 };
 
-module.exports = {print, resetGame}
+const playerMove = (playerChoice) => {
+	document.getElementById("message").innerHTML = "You chose " + playerChoice;
+	document.getElementById("reset").classList.remove("hidden");
+	const playButtons = document.getElementsByClassName("play-choice");
+	console.log(playButtons)
+	// for each (const button in playButtons) {
+	// 	button.classList.add("hidden");
+	// }
+}
+
+module.exports = {print, resetGame, playerMove}
