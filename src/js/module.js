5e6fd6b95f9381ebcff46cddb39f7b589c942394
Hoonio <hoonio@hoonio.com>
2017-04-03 23:36:13 +0100

    compute game result

--- src/js/module.js
+++ src/js/module.js
@@ -8,9 +8,10 @@ const resetGame = () => {
 };
 
 const playerMove = (playerChoice, choiceValue) => {
-	document.getElementById("message").innerHTML = "You chose " + playerChoice;
+	const computerChoice = generateComputerMove()
+	document.getElementById("message").innerHTML = compareHands(choiceValue, computerChoice);
 	document.getElementById("player-move").innerHTML = "You chose " + choiceValue;
-	document.getElementById("computer-move").innerHTML = "Computer chose " + generateComputerMove();
+	document.getElementById("computer-move").innerHTML = "Computer chose " + computerChoice;
 
 	toggleButtons();
 };
@@ -22,6 +23,20 @@ const toggleButtons = () => {
 		button.classList.toggle("hidden");
 	}
 };
+
+const compareHands = (playerHand, computerHand) => {
+	if (playerHand == 0 && computerHand == 2){
+		return "You won";
+	} else if (playerHand == 2 && computerHand == 0){
+		return "You lose";
+	} else if (playerHand > computerHand){
+		return "You won";
+	} else if (playerHand < computerHand){
+		return "You lose";
+	} else {
+		return "It's a tie"
+	}
+}
 
 const generateComputerMove = () => {
 	return Math.floor(3 * Math.random());
